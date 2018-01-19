59fc480df5db329c518bd2c12cac0c842dd14370
Andrian Putra <andribenjul@gmail.com>
2018-01-19 17:53:40 +0700

    started basic layout

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  // Hamburger toggle menu
+  $('.nav-toggle').click(function() {
+    $('.main-nav').toggleClass('is-open');
+    $('.hamburger').toggleClass('is-open');
+
+  })
+})
